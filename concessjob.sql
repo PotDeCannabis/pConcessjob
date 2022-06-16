8c328d169445f6663611b81a488718f4b81ca8e4
Pots <91101609+PotDeCannabis@users.noreply.github.com>
2022-06-16 17:09:42 +0200

    Update concessjob.sql

--- concessjob.sql
+++ concessjob.sql
@@ -14,7 +14,7 @@ INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_
 ('concess', 0, 'recrue', 'Recrue', 20, '', ''),
 ('concess', 1, 'novice', 'Novice', 30, '', ''),
 ('concess', 2, 'experimenter', 'Expérimenter', 40, '', ''),
-('concess', 3, 'medecin_chef', "Chef de Magasin", 60, '', ''),
+('concess', 3, 'chef_magasin', "Chef de Magasin", 60, '', ''),
 ('concess', 4, 'boss', 'Directeur', 80, '', '');
 
 INSERT INTO `jobs` (name, label) VALUES
